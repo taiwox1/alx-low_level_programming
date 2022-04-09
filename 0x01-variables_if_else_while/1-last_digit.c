422de4f5d92460aea5250880c9ed20921fc89d6b
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 00:52:40 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,9 @@ int main(void)
 	int n;
 	int last;
 
-
-	srand(time(0));	
-	
+	srand(time(0));		
 	n = rand() - RAND_MAX / 2;
-
 	last = n % 10;
-
 
 	if (last > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
