c81ac107d82cd458622b8b99ae39a0b9e10f286a
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 02:06:16 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet in lower case
+ * return: always zero
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+
+	return (0);
+}
