1cb0d2da977d6b2d64c4afaab4ee3b45b470431e
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 19:33:36 +0100

    Low level programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(n);
+			putchar(m);
+			putchar(',');
+			putchar(' ');
+
+		};
+
+
+
+	}
+	return (0);
+}
