e1964643a91e85b1036bb28d075f2668e4f6589b
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 02:54:28 +0100

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,11 @@
 int main(void)
 {
 	char n;
-	
 	for (n = 'a'; n <= 'z'; n++)
 		putchar(n);
-	
 	for (n = 'A'; n <= 'Z'; n++)
 		putchar(n);
-	
 	putchar('\n');
-
-        return (0);
+	
+	return (0);
 }
