a31a499e460341f84390bb7a6e70582faf8962c3
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 09:14:41 +0100

    print number of base 6

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,19 @@
-[200~#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - main block
- *   * Description: prints all single digit numbers of base 10
- *    * starting from 0, followed by a new line.
- *     * Return: 0
- *      */
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-	int c = 0;
-	while (c < 10)
+	int m = 0;
+	
+	while (m < 10)
 	{
-		putchar(48 + c);
-		if (c != 9)
+		putchar(48 + m);
+		if (m != 9)
 		{
 			putchar(',');
 			putchar(' ');
