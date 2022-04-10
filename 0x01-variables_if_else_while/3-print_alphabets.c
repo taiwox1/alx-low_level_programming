433997fc1c04f078202bf019e8252faf8b956b89
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 02:42:51 +0100

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,12 @@
 int main(void)
 {
         char n;
-	char m;
 
         for (n = 'a'; n <= 'z'; n++)
                 putchar(n);
 
-	for (m = 'A'; m <= 'Z'; m++)
-		putchar(m);
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
 
 	putchar('\n');
 
