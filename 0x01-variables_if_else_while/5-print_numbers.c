9f02eb252f4f4df915e7bbac95fc0b9241f07a76
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 03:44:49 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphabet in lower case and upper case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        int n;
+
+        for (0 = 1; n <= 9; n++)
+        {
+                print(%d, n);
+        };`
+        return (0);
+}
+
