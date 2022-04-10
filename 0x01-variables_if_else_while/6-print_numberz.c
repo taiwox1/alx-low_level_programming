646c08602c8f3660a6e9d0c52b60d4b6a2738912
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 04:16:42 +0100

    Print number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-        int n;
-
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-        };
+	int n;
+	
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	};
 	putchar('\n');
-        return (0);
+	return (0);
 
 }
 
