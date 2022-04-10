63c111fef08b769b63df7c943e6351ef21be87af
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 04:29:21 +0100

    Print number

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphabet in lower case and upper case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char n;
+
+        for (n = 'z'; n <= 'a'; n++)
+        {
+		putchar(n);
+        };
+        putchar('\n');
+        return (0);
+
+}
+
