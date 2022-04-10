669ebcb0931d2d7cbb0f1ff8b0b5e6b6fc6f671b
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 21:50:18 +0100

    low level programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,19 @@ int main(void)
 {
 	int n;
 	int m;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		for (m = (n + 1) ; m <= '9'; m++)
 		{
-				putchar(n);
-				putchar(m);
-				if (m != '9' || n != '8')
-				{
+			putchar(n);
+			putchar(m);
+			if (m != '9' || n != '8')
+			{
 				putchar(',');
 				putchar(' ');
-				}
-
+			}
 		};
-
-
-
 	}
 	putchar('\n');
 	return (0);
