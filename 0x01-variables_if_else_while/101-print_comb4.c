5fa6c021000b226198efe39b757ed8b94266b11e
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-10 23:19:16 +0100

    alx-low_level_programming

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-        int n;
-        int m;
+	int n;
+	int m;
 	int y;
-
-        for (n = '0'; n <= '9'; n++)
-        {
-                for (m = (n + 1) ; m <= '9'; m++)
-                {
+	
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = (n + 1) ; m <= '9'; m++)
+		{
 			for (y = (m + 1) ; y <= '9'; y++)
 			{
-			       	putchar(n);
+				putchar(n);
 				putchar(m);
 				putchar(y);
 				if (m != '8' || n != '7' || y != '9')
@@ -26,9 +26,9 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-                        }
-                };
-        }
-        putchar('\n');
-        return (0);
+			}
+		};
+	}
+	putchar('\n');
+	return (0);
 }  
