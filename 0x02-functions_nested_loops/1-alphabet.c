0a9b02135398465187d12c111e2f0ea8111292da
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-12 21:52:35 +0100

    functions nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,6 +7,9 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	print_alphabet("abcdefghijklmnopqrstuvwxyz");
+	print_alphabet('a');
+	print_alphabet('b');
+	print_alphabet('c');
+	print_alphabet('d');
 	return (0);
 }
