58a2875bde848a1abfaf284bb095af6c63120865
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-12 21:46:00 +0100

    functions nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "mian.h"
+/**
+ *main - fu
+ *
+ *
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	print_alphabet("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
