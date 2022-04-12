c6db36bc35e8abda3750f634bed434e0d7a837a4
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-12 20:52:17 +0100

    functions nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - function is meant to print putchar , followed by a new line
+ *
+ * Return: Always return 0.
+ */
+int main(void)
+{
+	print_alhabet_x10("abcdefghijklmnopqrstuvwsyz");
+	return (0);
+}
