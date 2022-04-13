eaee43bb7f6234690c124eb4f962d2989bbe49fd
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 01:41:05 +0100

    functions_nested-loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * _islower - functions checkes for lowercase chareacter
+ * _isalpha - functions checkes for lowercase chareacter
  *@c: letter being tested
  * Return: Always retune 0 or 1.
  */
 int _isalpha(int c)
 {
-        int value = c;
-
-        if ((value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z'))
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	int value = c;
+	if ((value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
