f1ab2dbbb0cb90f890b3ba8d7976c74dc75f80b7
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-14 00:26:24 +0100

    function, nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,11 @@ void print_times_table(int n)
 	int a;
 	int b;
 	int r;
+	int j = n;
 
-	for (a = 0; a <= n; a++)
+	for (a = 0; a <= j; a++)
 	{
-		for (b = 0; b <= n; b++)
+		for (b = 0; b <= j; b++)
 		{
 			if ( b < 0 || b > 15)
 			{
