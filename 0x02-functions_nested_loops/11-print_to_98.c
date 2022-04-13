f3c4d6d05b273eec00e1146677ac5606ab05774f
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 05:07:32 +0100

    functiont, nested loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - print n to 98
+ * @n: an integer to add to b
+ * Return: Void.
+ */
+
+int add(int n)
+{
+	for (;;){
+	if ( n >= 0 || n < 98 )
+	{
+		n++;
+		_putchar(n);
+	}
+	else if ( n = 98)
+	{
+		_putchar(n);
+	}
+	else if ( n > 98; && n != 98)
+	{
+		_putchar(n);
+		n--;
+	}
+	else if ( n < 0 && n != 98)
+	{
+		_putchar(n);
+		n++;
+	}
+	}
+
+
+}
