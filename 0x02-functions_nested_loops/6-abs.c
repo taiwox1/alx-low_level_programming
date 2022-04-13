afe80b5c2de948059cbe61a56c8d9e3fd3f8da9c
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 02:25:51 +0100

    functions_nested-loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute the absolute  value of an integer
+ * @n: argument
+ * Return: Always retunrn interger
+ * */
+int _abs(int n)
+{
+	int value = n^2;
+	int absol = sqrt(value);
+}
