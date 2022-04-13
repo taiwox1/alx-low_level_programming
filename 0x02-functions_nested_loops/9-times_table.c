032c1baf8ffc6cf49d86b35241f50f0ed1dfe90c
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 04:33:48 +0100

    functiont, nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,28 @@
 */
 void times_table(void)
 {
-	int x, y, multiplied_value;
+	int x, y, m;
 	
 	for (y = 0; y < 10; y++)
 	{
 		for (x = 0; x < 10; x++)
 		{
-			multiplied_value = x * y;
+			m x * y;
 			if (x == 0)
 			{
-				_putchar(multiplied_value + '0');
+				_putchar(m + '0');
 			}
-			else if (multiplied_value >= 10)
+			else if (m >= 10)
 			{
 				_putchar(' ');
-				_putchar(multiplied_value / 10 + '0');
-				_putchar(multiplied_value % 10 + '0')
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(multiplied_value + '0');
+				_putchar(m + '0');
 			}
 			if (x != 9)
 			{
