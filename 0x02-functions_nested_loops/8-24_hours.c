3dbba24d527437a77d88ad58bf5efd1972735d1e
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 03:50:00 +0100

    functiont, nested loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - function print every minute of Jack Bauer life
+ *
+ * Return: Alway void
+ */
+void jack_bauer(void)
+{
+	int a,b,c,d;
+	for (a = 0; a <= 2; a++)
+	{
+		for (a = 0; a <= 3; a++)
+		{
+			for (a = 0; a <= 5; a++)
+			{
+				for (a = 0; a <= 9; a++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(:);
+					_putchar(c);
+					_putchar(d);
+					_putchar("\n");
+				}
+			}
+		}
+
+	}
+
+}
