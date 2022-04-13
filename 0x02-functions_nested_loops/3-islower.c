897bcc4b5bb8698aa8d970641e50663a783d4e19
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-13 01:33:26 +0100

    functions_nested-loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - functions checkes for lowercase chareacter
- * @c: letter being tested
+ **@c: letter being tested @c: letter being tested
  * Return: Always retune 0 or 1.
  */
 int _islower(int c)
