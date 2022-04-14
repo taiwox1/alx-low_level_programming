86a393daf717d7ae41fdcdc0575b3c71373c83d6
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-14 20:16:32 +0100

    check the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 number
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -24,6 +24,8 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
+
 }
+
