5f97dcff7efb0b1c5c317dac502625496e593727
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-14 17:52:18 +0100

    what cause infinite loop?

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 intergers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+	
+	largest = largest_number(a, b, c);
+
+	print("%disthe leargest number\n", largest);
+
+	return (0);
+}
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+	
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && a >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c ;
+	}
+
+	return (largest);
+}
