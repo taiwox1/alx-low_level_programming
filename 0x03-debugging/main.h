cc625d5cb0032f521bf77d37a9c122b626040977
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-14 15:40:11 +0100

    debugging: test for positive and negstive

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-#endif
+
+#endif /* MAIN_H */
