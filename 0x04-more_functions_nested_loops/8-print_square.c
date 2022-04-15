fa1652f376811229f63ed706bab2d62051d4ec06
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-16 00:09:09 +0100

    Print straight line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_square(int n)
+{
+
+	int m = 0;
+	int k;
+
+	while (i < n && n > 0)
+	{
+		m = 0;
+		while (ii < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
+}
