77976720d9777a524462c87deec772db431ef93b
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 23:04:08 +0100

    Print straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,11 @@
 
 void print_line(int n)
 {
-	int c;
+	int c = 0;
 
-	for (c = 0; c <= n; c++)
+	while (i < n && n > 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
