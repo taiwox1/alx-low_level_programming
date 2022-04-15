be4a785a435a1aa51b29fe8c9959a066315ff7e4
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 14:31:56 +0100

    check for upper character

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isdigit - checks for a digit ( 0 through 9)
  * @c: c character to check
@@ -8,7 +7,7 @@
 
 int _isdigit(int c)
 {
-	if (c <= 0 && c >= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
