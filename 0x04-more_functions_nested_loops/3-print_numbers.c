adc565ddac7ccb65ccb1b7c30b16596262e0dde5
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 15:25:52 +0100

    multiply two number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print 0 to 9
+ * Return: num
+ */
+
+int print_numbers(void)
+{
+	int c;
+
+	for (c >= '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+		return (c);
+	}
+}
