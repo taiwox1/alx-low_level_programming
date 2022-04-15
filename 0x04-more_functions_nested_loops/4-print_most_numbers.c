f316e2043c73b7e9030499a197fa047c686d2708
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 19:11:36 +0100

    print number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return: Always 0 
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+	n = 0;
+	for ( n >= 0; n <= 9; n++)
+	{
+		if( n = 2 && n = 4)
+		{
+		}
+		else
+		{
+			_putchar(n);
+		}
+	}
+}
