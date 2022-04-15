fc105df6e68c1038b8c0e0cbcad73c389ae2829a
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 20:21:09 +0100

    print numbers but not 2 ans 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,13 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
+
 		if (n != 2 && n != 4)
 		{
-			_putchar(n + '0');
+			_putchar('0' + n);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
+
+
 }
