9da1a5a6e249f3a7bcc647dea1682ea49bf1701b
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 18:09:48 +0100

    print number 0 t0 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	int c;
+        int n;
 
-	for (c >= '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+        for (n >= '0'; n <= '9'; n++)
+        {
+                _putchar(n);
+        }
+        _putchar('\n');
 }
