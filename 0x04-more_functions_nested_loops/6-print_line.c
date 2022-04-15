d681c50d3faa4b7e5ce54ce6a26aee2cbcd591dc
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 22:37:31 +0100

    Print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - to prin straight line
+ * 
+ * Return: 0;
+ */
+
+void print_line(int n)
+{
+	int c;
+	int b;
+	b = n;
+	for (c = 0; c <= b ; c++)
+	{
+		if ( b < 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+}
