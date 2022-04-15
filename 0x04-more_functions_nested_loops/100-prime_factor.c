bacf1571cd454e37d510fa61aec5fee7ca68a8e5
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-16 00:43:04 +0100

    not just symbols

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for a digit
+ * @size : integer type
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+
+	int m = 1;
+	int k;
+
+	while (m <= size && size > 0)
+	{
+		k = 0;
+		while (m < size - m)
+		{
+			_putchar(' ');
+			k++;
+		}
+		k = 0;
+		while (k < m)
+		{
+			_putchar('#');
+			k++;
+		}
+
+		_putchar('\n');
+		m++;
+	}
+	if (m == 1)
+		_putchar('\n');
+}
