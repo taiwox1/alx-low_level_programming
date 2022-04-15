eed52b3189b55695f42cbe5962050c568ae00c64
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-15 19:18:33 +0100

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * print_numbers - Prints number
  *
  * Return: Always 0
  */
+
 void print_numbers(void)
 {
         int n;
-	n = 0;
 
-        for (n >= 0; n <= 0; n++)
+        for (n = 0; n < 10; n++)
         {
                 _putchar(n + '0');
         }
