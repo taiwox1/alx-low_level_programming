b851a2e7754e1e4fe4a7ec8099dbefc08592b7ac
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-21 06:10:08 +0100

    print lenght

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,16 +7,10 @@
 int _strlen(char *s)
 {
 	int a;
-	int b;
-	char c;
-	
-	b = s[0];
-	while ( b != '\0')
+	a = 0;
+	while (s[a])
 	{
-		c++;
 		a++;
-		b = s[a];
-		
 	}
-	return (c);
+	return (a);
 }
