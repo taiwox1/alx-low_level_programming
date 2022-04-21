71305e78f3934d2cf93f1088e477cc02d2bf55ea
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-21 03:57:05 +0100

    pointer: swap two values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap-int - function swap value of "a" and 'b'
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Always void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int intial;
+	intial = *a;
+	*b = *a;
+	*a = *b;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _swap
+ */
+
+int _strlen(char *s)
+{
+}
