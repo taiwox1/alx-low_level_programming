02a7a79497fa54152caa61e1fb3b2d4a2ff2d8ed
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-21 03:20:31 +0100

    pointer: update value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 0-reset_to_98 - update the value of pointer parameter to 98
+ * reset_to_98 - update the value of pointer parameter to 98
  * @n: parameter to update
  * Return: void
  */
