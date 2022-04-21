7dc766e200db866612efb53911c5195451550da1
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-21 06:15:32 +0100

    print lenght

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,14 @@
 /**
  * _strlen - return the lenght of a string;
  * @s: parameter of string to measure;
+ * Return: Always return 'a'
  */
 
 int _strlen(char *s)
 {
 	int a;
-	
+
+
 	a = 0;
 	while (s[a])
 	{
