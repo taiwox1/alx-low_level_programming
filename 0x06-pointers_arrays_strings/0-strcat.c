946c9e5c3c4fc427724bec959159a114eb612796
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-24 16:58:41 +0100

    concatenate two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,19 @@
  * *_strncat - this function concatenate two string together
  * @*dest: first pointer added to and finally return
  * @*src: second pointer holding string to be add.
- * @n: byte size
  * Return: void
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len;
 	int i;
 
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 		;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[dest_len + i] = src[i];
 	}
-	dest[dest_len + i] = '\0';
 	return (dest);
 }
