764ebb5bebe4848683d5f807eca8aac0c33056e2
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-24 20:37:54 +0100

    compare two string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strtings
+ * @s1: first string
+ * @s2: second string
+ * Return: void
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; (s1[i] != '\0') || (i < s2[1] != '\0'); i++)
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	return (0);
+}
