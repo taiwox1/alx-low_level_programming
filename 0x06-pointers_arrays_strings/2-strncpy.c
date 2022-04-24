6ab3291ca3507a4fb9f6816623f5632813bfb4e1
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-24 18:57:56 +0100

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * strncpy - function copy a string
+ * @dest: final destination
+ * @src: source
+ * @n: numbers of byte
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i + 1];
+	}
+	return (dest);
+}
