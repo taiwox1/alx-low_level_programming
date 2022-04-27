3f54a26bb6049434869da98c69e2153a20b35c96
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-27 03:52:08 +0100

    returns numbers of initial segment

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length substring
+ * @s: string
+ * @accept: check byte
+ * Return: n of byte;
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] > '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+	}
