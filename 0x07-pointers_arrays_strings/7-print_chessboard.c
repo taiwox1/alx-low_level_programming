8463c87dc66a38bff3ccd3e2d851d9b0107611ea
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-27 05:20:21 +0100

    low_level_programming

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,7 +5,6 @@
  * Return: void
  */
 
-
 void print_chessboard(char (*a)[8])
 {
 	int i;
@@ -15,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(*(i + a) + j));;
 		}
 		_putchar('\n');
 	}
