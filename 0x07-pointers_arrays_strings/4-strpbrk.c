9204f24afe366fb9cf630d3fddaf082bc35e34b0
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-04-27 04:50:11 +0100

    locate first occurence

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,16 +11,18 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
+	char p;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		for (j = 0; accept[j] > '\0'; j++)
 		{
-			if (s[i] != accept[j])
+			if (s[i] == accept[j])
 			{
-				return (s + i);
+				p = &s[i];
+				return (0);
 			}
 		}
 	}
-	return ('\0');
+	return (0);
 }
