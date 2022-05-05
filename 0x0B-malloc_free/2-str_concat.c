b318a6c1e6c77116ad556f5da7f99e98efffa2ae
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-05 21:35:51 +0100

    malloc and free function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - returns a pointer to a 2d array.
+ * @width: int
+ * @height: int
+ * Return:  array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **y, i, j;
+	y = malloc(sizeof(*y) * height);
+	if (width <= 0 || height <= 0 || y == 0)
+	return (NULL);
+
+else
+{
+	for (i = 0; i < height; i++)
+	{
+	
+		y[i] = malloc(sizeof(**y) * width);
+		if (y[i] == 0)
+		{
+			while (i--)
+				free(y[i]);
+			free(y);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			y[i][j] = 0;
+	}
+}
+return (y);
+}
