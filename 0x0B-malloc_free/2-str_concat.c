9cac5b4a157d0d0faee2ae86e50a0684953a591f
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-05 22:06:36 +0100

    malloc function in c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,8 +26,11 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
+	int size1;
+	int size2;
+	int i;
 	char *m;
+
 	if (s1 == NULL)
 		s1 = "\0";
 	if (s2 == NULL)
