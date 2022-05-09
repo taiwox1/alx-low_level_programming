3ddaf9d28ac28bdc27b28688bab728fa2d022e1a
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-10 00:21:37 +0100

    More malloc free

---
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - this function allocate memory using malloc
+ * @b - stores the memory allocated
+ * Return - void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *x;
+
+	x = malloc(sizeof(b));
+	
+	if (x == NULL)
+		exit (98);
+}
