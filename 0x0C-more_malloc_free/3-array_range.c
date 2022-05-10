f2d184c341d6c4f4c2cd2ac05fe9d64fda60d382
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-10 04:34:00 +0100

    malloc ,free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (0);
-	a  =  malloc((min - max + 1) * sizeof(int));
+	a  =  malloc((max - min + 1) * sizeof(int));
 	if (!a)
 		return (NULL);
 	x = min;
