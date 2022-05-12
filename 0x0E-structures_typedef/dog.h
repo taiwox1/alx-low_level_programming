7aa7ab57e57433f679007516c4dd5b5f8f43daa8
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-12 01:06:50 +0100

    struct typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog;
