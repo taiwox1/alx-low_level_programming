c2758af05b874006174b0ab1d6e331ab1de1f41c
Adedotun Taiwo <taiwoadedotunx1@gmaik.com>
2022-05-15 23:43:56 +0100

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
-/**t
+/**
  * print_name - function print name
  * @name: character to print
  * @f: function pointer
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
