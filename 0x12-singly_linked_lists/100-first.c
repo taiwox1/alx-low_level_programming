144cc2a1dfadec7d186c3f028ed04e828457a0c1
taiwox1 <taiwoadedotunx1@gmail.com>
2022-05-24 20:25:44 +0100

    linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,7 @@
  */
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf(
+			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+			);
 }
