c39530ff1ce126453c3d4d504cface7efa200d4d
taiwox1 <taiwoadedotunx1@gmail.com>
2022-05-28 00:09:09 +0100

    linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - print all element of a list
+ * @head: head pointer to list
+ * @n: int data to add
+ * Return: number of element in the list.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *temp;
+
+	temp  = (listint_t *)malloc(sizeof(listint_t));
+
+        if (temp == NULL)
+        {
+                free(temp);
+                return (NULL);
+        }
+        temp->n = n;
+        temp->next = NULL;
+
+	ptr = *head;
+	while (head != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	return (temp);
+}
