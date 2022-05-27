07a74b1f61af976a65df1d62ed3ade9c4125ddd2
taiwox1 <taiwoadedotunx1@gmail.com>
2022-05-27 21:51:27 +0100

    more linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        while (h != NULL)
-        {
-
-                h = h->next;
-                i++;
-        }
-        return i;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
