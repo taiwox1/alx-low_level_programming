f22cb887169c32566091d85180b22a302283dc79
taiwox1 <taiwoadedotunx1@gmail.com>
2022-05-27 23:36:30 +0100

    lined litst

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - print all element of a list
+ * @head: head pointer to list
+ * @n: int data to add
+ * Return: number of element in the list.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node  = (listint_t *)malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->next = *head;
+	*head  = new_node;
+	return (new_node);
+}
