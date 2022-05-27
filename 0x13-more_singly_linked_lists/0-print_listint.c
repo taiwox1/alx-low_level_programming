9db70d98cdc35ed13d61850ff67a9a54f52bc5db
taiwox1 <taiwoadedotunx1@gmail.com>
2022-05-27 21:31:10 +0100

    more linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	int i = 0;
 
-	while(h != NULL)
-       	{
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	return i;
+	return (i);
 }
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - print all element of a list
+ * @h: head pointer to list
+ * Return: number of element in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+        int i = 0;
+
+        while(h != NULL)
+        {
+
+                h = h->next;
+                i++;
+        }
+        return i;
+}
+
