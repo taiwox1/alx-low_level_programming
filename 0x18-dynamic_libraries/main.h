9ede2f2bf995da40346a532490985d0cf607f974
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-13 04:34:12 +0100

    shared library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int nrandom()
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
---
+++ 0x18-dynamic_libraries/randnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nrandom()
+{
+	int i,n;
+	n = 5;
+	/* Print 5 random numbers from 1 to 75 */
+	for( i = 1 ; i < n ; i++ ) {
+	       return (rand() % 76);
+	}	
+}
