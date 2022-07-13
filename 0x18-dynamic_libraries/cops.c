3c5f846e7320b030e3582b23595675e1b171b663
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-13 02:41:43 +0100

    c and python sharing library is fun

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always interger
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always interger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: always interger
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - find mode of a numbers
+ * @a: first number
+ * @b: second number
+ * Return: always interger
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
