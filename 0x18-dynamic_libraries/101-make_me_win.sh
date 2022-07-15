595f8ab47bfbeaebf952d38e76ed041f97953b76
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-15 23:36:11 +0100

    dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://github.com/taiwox1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/randnum.so
-export LD_PRELOAD =/tmp/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
