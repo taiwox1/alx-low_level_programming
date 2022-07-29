01755285ead2e70b2a19aaa2548fbe928abb139f
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-29 14:47:18 +0100

    hash table

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -11,8 +11,9 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int c;
 
-	hassh = 5381;
+	hash = 5381;
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
 	return (hash);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,7 +2,7 @@
 #define HASH_TABLE_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
