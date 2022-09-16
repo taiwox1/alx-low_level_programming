9dbf7efc5664c9eb4a83b19981c40fc3fe466022
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-09-16 02:23:13 +0100

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """ The function returns the perimeter of an island
 discriped in grid
 """
 
+
 def island_perimeter(grid):
+
     """Grid(int) : list of interger 0 and 1
     0: water zone
     1: land zone
@@ -11,16 +14,15 @@ def island_perimeter(grid):
     """
     row_len = len(grid)
     col_len = len(grid[0])
-        #print(range(len(grid))) 
     size = 0
     edge = 0
-        
+
     for x in range(row_len):
         for y in range(col_len):
             if grid[x][y] == 1:
-                    size += 1
-            if (y > 0 and grid[x][y] ==  1 and grid[x][y - 1] ==  1):
-                    edge += 1
-            if (x > 0 and grid[x][y] ==  1 and grid[x - 1][y] ==  1):
-                    edge += 1
-    return ((size*4) - (edge *2))
+                size += 1
+            if (y > 0 and grid[x][y] == 1 and grid[x][y - 1] == 1):
+                edge += 1
+            if (x > 0 and grid[x][y] == 1 and grid[x - 1][y] == 1):
+                edge += 1
+    return ((size * 4) - (edge * 2))
